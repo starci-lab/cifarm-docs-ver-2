6d253d9eb3bf840456c3cc7815507c92622ccadb
uydev <lequocuyit@gmail.com>
2025-05-24 15:47:35 +0700

    Refactor ESLint configuration to use FlatCompat for Next.js integration and update project name in package files.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,16 @@
-import globals from "globals"
-import pluginJs from "@eslint/js"
-import tseslint from "typescript-eslint"
-import pluginReact from "eslint-plugin-react"
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-    { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-    { languageOptions: { globals: globals.browser } },
-    pluginJs.configs.recommended,
-    ...tseslint.configs.recommended,
-    pluginReact.configs.flat.recommended,
-    {
-        rules: {
-            "react/display-name": "off",
-            indent: ["error", 4],
-            "react-hooks/exhaustive-deps": "off",
-            "linebreak-style": "off",
-            quotes: ["error", "double"],
-            semi: ["error", "never"],
-        },
-    },
-]
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+];
+
+export default eslintConfig;
