1d0adf58d32afba337667ba58f6cfa152676449a
uydev <lequocuyit@gmail.com>
2025-05-25 07:51:30 +0700

    Refactor GrowthStages component usage in crop and fruit guides
    - Updated the `GrowthStages` component to accept `type` and `name` props instead of `cropName`, enhancing flexibility for both crops and fruits.
    - Modified all relevant crop guide pages to reflect the new `GrowthStages` prop structure, ensuring consistency across guides.
    - Added new fruit guides for "Dragon Fruit," "Jackfruit," "Pomegranate," and "Rambutan," incorporating the updated `GrowthStages` component for better clarity on growth stages.

--- app/_components/growth-stages.tsx
+++ app/_components/growth-stages.tsx
@@ -1,21 +1,22 @@
 import Image from 'next/image'
 
 interface GrowthStagesProps {
-  cropName: string;
+  type: 'crop' | 'fruit';
+  name: string;
   stages: number;
 }
 
-export const GrowthStages = ({ cropName, stages }: GrowthStagesProps) => {
+export const GrowthStages = ({ type, name, stages }: GrowthStagesProps) => {
   return (
     <div className="flex flex-row gap-2 w-full mt-10">
       {Array.from({ length: stages }, (_, i) => i + 1).map((stage) => (
         <div key={stage} className="w-[20%]">
           <Image
-            src={`/crops/${cropName}/${stage}.png`}
-            alt={`${cropName} Stage ${stage} 2`}
+            src={`/${type}s/${name}/${stage}.png`}
+            alt={`${name} Stage ${stage} 2`}
             width={200}
             height={200}
-            className="w-full h-auto"
+            className="w-full h-[200px] object-contain"
           />
         </div>
       ))}
